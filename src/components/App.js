0cddf496f8f23cb0d736fe16ba18fe782e01f03a
Uche Ozoemena <ucheozoemena@gmail.com>
2018-12-04 16:36:27 +0100

    Make AJAX request with search term using axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID c24ece56593157a034bad2a8fd9dc65d8751d3bd93d2de40d5d9bc047f29a7dc'
+      }
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
